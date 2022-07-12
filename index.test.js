c0224ebc9e4be345e7c0c211ee0816d583c773b4
khaled88ebdelli <khaled88ebdelli@gmail.com>
2022-07-12 20:41:30 +0200

    add tests

--- index.test.js
+++ index.test.js
@@ -2,56 +2,31 @@ const { drinkMaker, payMoney } = require("./index");
 const commands = require("./commands");
 const prices = require("./prices");
 
-describe("Turn order into string instructions", () => {
-  test.skip("Should return C:1:0 when order is coffee with one sugar", () => {
-    const instructions = drinkMaker(commands.COFFEE, 1);
-    expect(instructions).toBe("C:1:0");
-  });
+describe('Turn order into string instructions', () => {
 
-  test.skip("Should return T:1:0 when order is tea with one sugar", () => {
-    const instructions = drinkMaker(commands.TEA, 1);
-    expect(instructions).toBe("T:1:0");
-  });
+    test('should return correct for hot coffee with 2 sugars', () => {
+        const instructions = payMoney(commands.COFFEE, 0.7, 2, true);
+        expect(instructions).toBe('Ch:2:0');
+    });
 
-  test.skip("Should return H:2:0 when order is chocolate with two sugars", () => {
-    const instructions = drinkMaker(commands.CHOCOLATE, 2);
-    expect(instructions).toBe("H:2:0");
-  });
+    test('should return correct for ORANGE', () => {
+        const instructions = payMoney(commands.ORANGE, 0.7);
+        expect(instructions).toBe('O::');
+    });
 
-  test.skip("Should return H:: when order is chocolate with no sugar (no stick)", () => {
-    const instructions = drinkMaker(commands.CHOCOLATE, 0);
-    expect(instructions).toBe("H::");
-  });
-});
+    test('should return money missing for ORANGE', () => {
+        const instructions = payMoney(commands.ORANGE, 0.5);
+        expect(instructions).toBe(prices.ORANGE_PRICE - 0.5 + 'money missing');
+    });
 
-describe("Turn order into string instructions", () => {
-  test("should return correct for coffee", () => {
-    const instructions = payMoney(commands.COFFEE, 0.7);
-    expect(instructions).toBe("accepted");
-  });
+    test('should return correct for hot chocolate with one sugar', () => {
+        const instructions = payMoney(commands.CHOCOLATE, 0.7, 1, true);
+        expect(instructions).toBe('Hh:1:0');
+    });
 
-  test("should return money missing for coffee", () => {
-    const instructions = payMoney(commands.COFFEE, 0.5);
-    expect(instructions).toBe(prices.COFFEE_PRICE - 0.5 + "money missing");
-  });
+    test('should return correct for hot tea with no sugar', () => {
+        const instructions = payMoney(commands.TEA, 0.7, 0, true);
+        expect(instructions).toBe('Th::');
+    });
 
-  test("should return correct for tea", () => {
-    const instructions = payMoney(commands.TEA, 0.5);
-    expect(instructions).toBe("accepted");
-  });
-
-  test("should return money missing for tea", () => {
-    const instructions = payMoney(commands.TEA, 0.3);
-    expect(instructions).toBe(prices.TEA_PRICE - 0.3 + "money missing");
-  });
-
-  test("should return correct for CHCOLATE", () => {
-    const instructions = payMoney(commands.CHOCOLATE, 0.5);
-    expect(instructions).toBe("accepted");
-  });
-
-  test("should return money missing for CHOCOLATE", () => {
-    const instructions = payMoney(commands.CHOCOLATE, 0.4);
-    expect(instructions).toBe(prices.CHOCOLATE_PRICE - 0.4 + "money missing");
-  });
 });
