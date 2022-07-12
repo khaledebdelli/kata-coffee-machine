5abc6b7f47a7bcabbf74323a346d50299bfc84e1
khaled88ebdelli <khaled88ebdelli@gmail.com>
2022-07-12 20:41:39 +0200

    implements response

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const commands = require("./commands");
 const prices = require("./prices");
 
-const drinkMaker = function (drink, sugars) {
-  if (sugars === 0) {
-    return drink + "::";
-  }
-  return drink + ":" + sugars + ":0";
+const drinkMaker = function (drink, sugars, temperature) {
+    let hot;
+    if (drink === commands.ORANGE) return commands.ORANGE + '::';
+
+    if (temperature === true) hot ='h'; else hot = '';
+
+    if (sugars === 0) {
+        return drink + hot + '::';
+    }
+    return drink + hot + ':' + sugars + ':0';
 };
 
-const payMoney = function (drink, money) {
-  if ((drink === commands.COFFEE && money >= prices.COFFEE_PRICE) || (drink === commands.TEA && money >= prices.TEA_PRICE) || 
-  (drink === commands.CHOCOLATE && money >= prices.CHOCOLATE_PRICE))
-    return "accepted";
+const payMoney = function (drink, money, sugars, temperature) {
+  if (
+    (drink === commands.COFFEE && money >= prices.COFFEE_PRICE) ||
+    (drink === commands.TEA && money >= prices.TEA_PRICE) ||
+    (drink === commands.CHOCOLATE && money >= prices.CHOCOLATE_PRICE) ||
+    (drink === commands.ORANGE && money >= prices.ORANGE_PRICE)
+  )
+    return drinkMaker(drink, sugars, temperature);
 
   if (drink === commands.COFFEE) return prices.COFFEE_PRICE - money + "money missing";
   if (drink === commands.TEA) return prices.TEA_PRICE - money + "money missing";
   if (drink === commands.CHOCOLATE) return prices.CHOCOLATE_PRICE - money + "money missing";
+  if (drink === commands.ORANGE) return prices.ORANGE_PRICE - money + "money missing";
 };
 
 module.exports = {
-    drinkMaker,
-    payMoney,
-}
+  drinkMaker,
+  payMoney
+};
