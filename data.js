d6aaefdd873833ee73d4616c730967774c857555
khaled88ebdelli <khaled88ebdelli@gmail.com>
2022-07-13 11:01:52 +0200

    create data

---
+++ data.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { drink: "C", price: 0.6 },
+  { drink: "C", price: 0.6 },
+  { drink: "C", price: 0.6 },
+  { drink: "H", price: 0.5 },
+  { drink: "O", price: 0.6 },
+  { drink: "O", price: 0.6 },
+  { drink: "T", price: 0.4 },
+  { drink: "H", price: 0.5 }
+];
