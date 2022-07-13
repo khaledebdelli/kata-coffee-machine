a5e266f5e75c02ae81d757a05a4829afd0b0c607
khaled88ebdelli <khaled88ebdelli@gmail.com>
2022-07-13 12:13:55 +0200

    add ressources

---
+++ resources.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    drink: "C",
+    ingredient: {
+      milk: 0.1,
+      water: 0.1
+    }
+  },
+  {
+    drink: "H",
+    ingredient: {
+      milk: 0.2,
+      water: 0.1
+    }
+  },
+  {
+    drink: "O",
+    ingredient: {
+      milk: 0,
+      water: 0.1
+    }
+  },
+  {
+    drink: "T",
+    ingredient: {
+      milk: 0,
+      water: 0.2
+    }
+  }
+];
