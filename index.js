32b2cea9d9d71a1648fa11dfba40fc98e7f3ab85
khaled88ebdelli <khaled88ebdelli@gmail.com>
2022-07-13 11:04:13 +0200

    implement response

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
-const commands = require("./commands");
-const prices = require("./prices");
+const data = require("./data");
 
-const drinkMaker = function (drink, sugars, temperature) {
-    let hot;
-    if (drink === commands.ORANGE) return commands.ORANGE + '::';
+const drinkMaker = function () {
+  let total = data.map( a => a.price).reduce( (x,y) => x+y, 0);
+  let coffee = drinkQuantity(data,'C');
+  let tea = drinkQuantity(data,'T');
+  let chocolate = drinkQuantity(data,'H');
+  let orange = drinkQuantity(data,'O');
 
-    if (temperature === true) hot ='h'; else hot = '';
-
-    if (sugars === 0) {
-        return drink + hot + '::';
-    }
-    return drink + hot + ':' + sugars + ':0';
+  return coffee +' coffee and ' + tea +' tea and ' + chocolate +  ' chocolate and ' + orange + ' orange with a total of '+ total;
 };
 
-const payMoney = function (drink, money, sugars, temperature) {
-  if (
-    (drink === commands.COFFEE && money >= prices.COFFEE_PRICE) ||
-    (drink === commands.TEA && money >= prices.TEA_PRICE) ||
-    (drink === commands.CHOCOLATE && money >= prices.CHOCOLATE_PRICE) ||
-    (drink === commands.ORANGE && money >= prices.ORANGE_PRICE)
-  )
-    return drinkMaker(drink, sugars, temperature);
-
-  if (drink === commands.COFFEE) return prices.COFFEE_PRICE - money + "money missing";
-  if (drink === commands.TEA) return prices.TEA_PRICE - money + "money missing";
-  if (drink === commands.CHOCOLATE) return prices.CHOCOLATE_PRICE - money + "money missing";
-  if (drink === commands.ORANGE) return prices.ORANGE_PRICE - money + "money missing";
-};
+function drinkQuantity(data,drink) {
+  return data.map(a=> a.drink).filter(a=>a===drink).length;
+}
 
 module.exports = {
   drinkMaker,
-  payMoney
 };
